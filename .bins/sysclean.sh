9f703e3fc1149985be9d86588990e54ae12b921d
stanleyedward <114278820+stanleyedward@users.noreply.github.com>
2025-08-21 15:08:43 +0530

    update cleaning script

--- .bins/sysclean.sh
+++ .bins/sysclean.sh
@@ -4,6 +4,24 @@ function sysclean {
     fi
     sudo pacman -Scc
     sudo pacman -Rns $(pacman -Qdtq)
-    echo "Clean-up done!"
+    yay -Sc
+    echo "Clean-up for system packages completed."
+
+    pip cache purge
+    conda clean --all -y
+    npm cache clean --force
+    echo "Clean-up for Python packages completed."
+
+    read -p "do you want to delete ~/.cache(y/n)?" answer
+
+    if [[ $answer == "y" || $answer == "Y" || $answer == "yes" || $answer == "Yes" ]]; then
+	rm -rf ~/.cache/*
+	echo "Cache directory cleaned."
+    else
+	echo "Cache directory not cleaned."
+    fi
+    
+    rm -rf ~/.local/share/Trash/files/*
+    rm -rf ~/.local/share/Trash/info/*
 }
 
